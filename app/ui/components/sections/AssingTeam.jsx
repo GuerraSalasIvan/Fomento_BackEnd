d8a5a5bf5a0d4bb2471a120285d52758ca39a72b
GuerraSalasIvan <guerrasalasivan@gmail.com>
2024-05-31 09:55:35 +0200

    pagina para asociar equipo con jugador

--- app/ui/components/sections/AssingTeam.jsx
+++ app/ui/components/sections/AssingTeam.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useAuth } from '@/hooks/auth';
+import { useRouter } from 'next/navigation';
 
 
 function AssingTeam() {
 
+    const router = useRouter()
     const { user } = useAuth({ middleware: 'guest' });
 
     return (
-        <div>AssingTeam
-            <p>{user ? user.name : 'No logueado'}</p>
+        <div>
+            {user ? (
+                user.player.teams && user.player.teams.length > 0 ? (
+                    router.push('/dashboard')
+                ) : (
+                    <p>Jugador sin equipo</p>
+                )
+            ) : (
+                <p>No logueado</p>
+            )}
         </div>
     )
 }
--- app/user/page.jsx
+++ app/user/page.jsx
@@ -6,10 +6,7 @@ import AssignTeam from '@/app/ui/components/sections/AssingTeam';
 export default function UserPage() {
     return (
         <div>
-            <h1>user</h1>
             <AssignTeam />
-
-            {/* <CreatePlayer /> */}
         </div>
     )
 }
