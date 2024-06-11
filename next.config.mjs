3e1bc2774f84a11ff07944b6c59599ae02a00fd2
GuerraSalasIvan <guerrasalasivan@gmail.com>
2024-06-11 17:50:38 +0200

    corregido url fotos

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'https://proyectointegrado-production-0e79.up.railway.app', '35.175.64.140', 'proyectointegrado-production-0e79.up.railway.app'],
+        domains: ['localhost', 'proyectointegrado-production-0e79.up.railway.app', '35.175.64.140'],
     },
 };
 
