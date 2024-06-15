34923b27f226cb94240eb81a34a2aabd7d7633ec
GuerraSalasIvan <guerrasalasivan@gmail.com>
2024-06-15 17:04:36 +0200

    fuciona login y registro ocn token

--- hooks/auth.js
+++ hooks/auth.js
@@ -1,129 +1,70 @@
-import useSWR from 'swr'
-import axios from '@/lib/axios'
-import { useEffect } from 'react'
-import { useParams, useRouter } from 'next/navigation'
+// useAuth.js
+
+import useSWR from 'swr';
+import axios from '@/lib/axios';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
-    const router = useRouter()
-    const params = useParams()
-
-    const csrf = async () => {
-        try {
-            const response = await axios.get('/sanctum/csrf-cookie');
-            console.log('CSRF Cookie Set:', response);
-        } catch (error) {
-            console.error('Error setting CSRF cookie:', error);
-        }
-    };
+    const router = useRouter();
 
     const fetchUser = async () => {
-        await csrf();
         return axios
-            .get('/api/user')
+            .get('/user')
             .then(res => res.data)
             .catch(error => {
-                if (error.response.status !== 409) throw error
-
-                router.push('/verify-email')
+                if (error.response.status !== 409) throw error;
+                router.push('/verify-email');
             });
     };
 
-    const { data: user, error, mutate } = useSWR('/api/user', fetchUser);
+    const { data: user, error, mutate } = useSWR('/user', fetchUser);
 
     const register = async ({ setErrors, ...props }) => {
-        await csrf()
+        setErrors([]);
 
-        setErrors([])
-
-        axios
-            .post('/register', props)
-            .then(() => mutate())
-            .catch(error => {
-                if (error.response.status !== 422) throw error
-
-                setErrors(error.response.data.errors)
-            })
-    }
+        try {
+            const response = await axios.post('/register', props);
+            const { access_token, user } = response.data;
+            localStorage.setItem('token', access_token); // Guardar el token en localStorage
+            mutate(user);
+        } catch (error) {
+            if (error.response.status !== 422) throw error;
+            setErrors(error.response.data.errors);
+        }
+    };
 
     const login = async ({ setErrors, setStatus, ...props }) => {
-        await csrf()
+        setErrors([]);
+        setStatus(null);
 
-        setErrors([])
-        setStatus(null)
-
-        axios
-            .post('/login', props)
-            .then(() => mutate())
-            .catch(error => {
-                if (error.response.status !== 422) throw error
-
-                setErrors(error.response.data.errors)
-            })
-    }
-
-    const forgotPassword = async ({ setErrors, setStatus, email }) => {
-        await csrf()
-
-        setErrors([])
-        setStatus(null)
-
-        axios
-            .post('/forgot-password', { email })
-            .then(response => setStatus(response.data.status))
-            .catch(error => {
-                if (error.response.status !== 422) throw error
-
-                setErrors(error.response.data.errors)
-            })
-    }
-
-    const resetPassword = async ({ setErrors, setStatus, ...props }) => {
-        await csrf()
-
-        setErrors([])
-        setStatus(null)
-
-        axios
-            .post('/reset-password', { token: params.token, ...props })
-            .then(response =>
-                router.push('/login?reset=' + btoa(response.data.status)),
-            )
-            .catch(error => {
-                if (error.response.status !== 422) throw error
-
-                setErrors(error.response.data.errors)
-            })
-    }
-
-    const resendEmailVerification = ({ setStatus }) => {
-        axios
-            .post('/email/verification-notification')
-            .then(response => setStatus(response.data.status))
-    }
+        try {
+            const response = await axios.post('/login', props);
+            const { accessToken, user } = response.data;
+            localStorage.setItem('token', accessToken); // Guardar el token en localStorage
+            mutate(user);
+        } catch (error) {
+            if (error.response.status !== 422) throw error;
+            setErrors(error.response.data.errors);
+        }
+    };
 
     const logout = async () => {
-        if (!error) {
-            await axios.post('/logout').then(() => mutate())
+        try {
+            await axios.post('/logout');
+            localStorage.removeItem('token'); // Eliminar el token de localStorage
+            mutate(null);
+            router.push('/login');
+        } catch (error) {
+            console.error('Logout failed:', error);
         }
-
-        window.location.pathname = '/login'
-    }
+    };
 
     const assignTeam = async ({ playerId, teamId }) => {
-        await csrf();
-        return axios.post('https://proyectointegrado-production-0e79.up.railway.app/api/assignTeam', {
-            playerId,
-            teamId
-        }, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            }
-        });
-    }
+        return axios.post('/assignTeam', { playerId, teamId });
+    };
 
     const updateProfile = async ({ name, full_name, email, birthdate, image, setErrors, setMessage }) => {
-        await csrf();
-
         const formData = new FormData();
         formData.append('name', name);
         formData.append('full_name', full_name);
@@ -134,7 +75,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         }
 
         try {
-            const response = await axios.post('/api/updateUser', formData, {
+            const response = await axios.post('/updateUser', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
@@ -148,25 +89,21 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
     };
 
     useEffect(() => {
-        if (middleware === 'guest' && redirectIfAuthenticated && user)
-            router.push(redirectIfAuthenticated)
-        if (
-            window.location.pathname === '/verify-email' &&
-            user?.email_verified_at
-        )
-            router.push(redirectIfAuthenticated)
-        if (middleware === 'auth' && error) logout()
-    }, [user, error])
+        if (middleware === 'guest' && redirectIfAuthenticated && user) {
+            router.push(redirectIfAuthenticated);
+        }
+        if (window.location.pathname === '/verify-email' && user?.email_verified_at) {
+            router.push(redirectIfAuthenticated);
+        }
+        if (middleware === 'auth' && error) logout();
+    }, [user, error]);
 
     return {
         user,
         register,
         login,
-        forgotPassword,
-        resetPassword,
-        resendEmailVerification,
         logout,
         assignTeam,
         updateProfile,
-    }
-}
+    };
+};
